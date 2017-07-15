ed552fa386e19fb3c8f9a2f0830a1ca945a2619b
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-07-15 10:57:39 +0100

    Add method to enter search term

--- src/test/java/com/cucumber/pages/GoogleHomePage.java
+++ src/test/java/com/cucumber/pages/GoogleHomePage.java
@@ -1,5 +1,8 @@
 package com.cucumber.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by robb on 14/07/2017.
  */
@@ -7,8 +10,8 @@ public class GoogleHomePage {
 
     private String searchFieldLocator = ".gsfi";
 
-    public void enterSearchTerm() {
+    public void enterSearchTerm(String searchTerm) {
         AbstractPage.waitForPageToLoad();
-
+        driver.findElement(By.id(searchFieldLocator)).click();
     }
 }
